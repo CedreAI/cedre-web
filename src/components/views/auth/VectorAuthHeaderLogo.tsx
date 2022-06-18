03da6ca3539a6c0e9510523daedc28eac54802f1
zandi2015 <zandi2015@gmail.com>
2022-06-18 20:31:41 +0430

    change mobile guide page ui

--- src/components/views/auth/VectorAuthHeaderLogo.tsx
+++ src/components/views/auth/VectorAuthHeaderLogo.tsx
@@ -23,7 +23,7 @@ export default class VectorAuthHeaderLogo extends React.PureComponent {
 
     render() {
         const brandingConfig = SdkConfig.get().branding;
-        let logoUrl = "themes/element/img/logos/element-logo.png";
+        let logoUrl = "https://static.fanoos.app/img/logo.png";
         if (brandingConfig && brandingConfig.authHeaderLogoUrl) {
             logoUrl = brandingConfig.authHeaderLogoUrl;
         }
